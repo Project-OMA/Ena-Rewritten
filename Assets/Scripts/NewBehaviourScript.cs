2338798f321a9c872dbc2baeebdbee715aa6d90d
tomast1337 <vycasnicolas@gmail.com>
2023-09-02 16:17:25 -0300

    Basic Package Structure

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var vec3 = new Vector3(1, 2, 3);
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
