2d0cec3c945304e508c1cd5588a60c2bfc6e392e
GabsNandes <gabs2019fernandes@gmail.com>
2023-09-09 19:28:09 -0300

    movimentação fps

--- Assets/Scripts/InteractionController/InteractionController.cs
+++ Assets/Scripts/InteractionController/InteractionController.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class InteractionController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float correr = 2, andar = 1;
+   private int velocidadeFrente;
+   private int velocidadeTras;
+
+   private int velocidadeLateral;
+
+   void Start (){
+      velocidadeFrente = 10;
+      velocidadeTras = 5;
+      velocidadeLateral = 2;
+   }
+
+   void Update () {
+    if(Input.GetKey("w")){
+         
+        transform.Translate(0,0,andar*velocidadeFrente*Time.deltaTime);
+            
+         
+    }
+    if(Input.GetKey("s")){
+         
+        transform.Translate(0,0,-andar*velocidadeTras*Time.deltaTime);
+      
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    if(Input.GetKey("d")){
+         
+        transform.Rotate(0,(-andar*velocidadeLateral*Time.deltaTime),0);
+            
+         
     }
+    if(Input.GetKey("a")){
+         
+        transform.Rotate(0,(andar*velocidadeLateral*Time.deltaTime),0);
+      
+    }
+
+      
+   }
 }
