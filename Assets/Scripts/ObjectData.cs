a5d431a543ffc5065292f1368e511fd10b44d9a1
Bentroen <bemcdc@gmail.com>
2023-09-09 21:32:33 -0300

    Change object system to allow multiple variations

--- Assets/Scripts/ObjectData.cs
+++ Assets/Scripts/ObjectData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ObjectData", menuName = "ScriptableObjects/ObjectData", order = 1)]
+
 public class ObjectData : ScriptableObject
 {
 
@@ -12,64 +13,50 @@ public class ObjectData : ScriptableObject
     [SerializeField]
     public List<ObjectEntry> Objects = new List<ObjectEntry>();
 
+
+    [System.Serializable]
+    public class ObjectVariation
+    {
+        public string id;
+        public int rotation;
+        public int offsetX;
+        public int offsetY;
+    }
+
     [System.Serializable]
     public class ObjectEntry
     {
-        public string id;
         public string name;
         public GameObject prefab;
+        public List<ObjectVariation> variations = new List<ObjectVariation>();
+    }
 
+    [System.Serializable]
+    public class ObjectPrefab
+    {
+        // Used as a return type for GetObject
+        public GameObject prefab;
         public int rotation;
+        public int offsetX;
+        public int offsetY;
     }
 
-    public GameObject GetObject(string id)
+    public ObjectPrefab GetObject(string id)
     {
-        if (Objects.Exists(x => x.id == id))
+        for (int i = 0; i < Objects.Count; i++)
         {
-            return Objects.Find(x => x.id == id).prefab;
+            if (Objects[i].variations.Exists(x => x.id == id))
+            {
+                ObjectVariation variation = Objects[i].variations.Find(x => x.id == id);
+                return new ObjectPrefab
+                {
+                    prefab = Objects[i].prefab,
+                    rotation = variation.rotation,
+                    offsetX = variation.offsetX,
+                    offsetY = variation.offsetY
+                };
+            }
         }
-        else
-        {
-            throw new System.Exception("Object " + id + " not found");
-        }
-    }
-
-    public GameObject GetDefaultObject()
-    {
-        if (Objects.Count > 0)
-        {
-            return Objects[0].prefab; // Return the first object in the list as the default
-        }
-        else
-        {
-            throw new System.Exception("No objects available in ObjectData");
-        }
-    }
-
-    public GameObject RegisterObject(string id, string name, GameObject prefab)
-    {
-        if (objectMap.ContainsKey(id))
-        {
-            throw new System.Exception("Object " + id + " already registered");
-        }
-        else
-        {
-            objectMap[id] = prefab;
-            Objects.Add(new ObjectEntry { id = id, name = name, prefab = prefab });
-            return prefab;
-        }
-    }
-
-    public void UnregisterObject(string id)
-    {
-        if (objectMap.ContainsKey(id))
-        {
-            objectMap.Remove(id);
-            Objects.RemoveAll(x => x.id == id);
-        }
-        else
-        {
-            throw new System.Exception("Object " + id + " not registered");
-        }
+        throw new System.Exception("Object " + id + " not found");
     }
 }
