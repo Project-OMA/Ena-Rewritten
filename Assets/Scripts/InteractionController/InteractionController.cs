964cd7287ff953277a59d3f6c1e3db91ccee4453
Bernardo Costa <bernardo.costa@aluno.cefet-rj.br>
2024-03-20 19:18:26 -0300

    fix: replace `Rigidbody` in player movement with `CharacterController`

--- Assets/Scripts/InteractionController/InteractionController.cs
+++ Assets/Scripts/InteractionController/InteractionController.cs
@@ -4,15 +4,16 @@ using UI;
 public class InteractionController : MonoBehaviour
 {
     public float walkDistance = 1.5f;
+    
+    float stepPeriod = 0.25f;
+    float nextStepTime = -1f;
 
-    public float runSpeed = 3;
+    public float runSpeed = 30;
     public GameObject cam;
     private Collider collider;
-    private Rigidbody rigidbody;
+    private CharacterController controller;
 
     public TTSManager ttsManager;
-    
-
 
     private float runningInput()
 
@@ -28,30 +29,52 @@ public class InteractionController : MonoBehaviour
         var control = new Vector3(y, x, 0);
 
         Vector3 right = cam.transform.right;
-        Vector3 forward = Vector3.ProjectOnPlane(cam.transform.forward, Vector3.up).normalized;
+        Vector3 forward = cam.transform.forward;
         Vector3 moveVector = forward * control.y + right * control.x;
         moveVector.y = 0;
 
-        return moveVector.normalized*walkDistance;
+        return moveVector.normalized * walkDistance * stepPeriod;
+    }
+
+    private void doStep() {
+        Debug.Log("Doing step at " + nextStepTime);
+        Vector3 moveVector = getMoveVector();
+        nextStepTime = Time.time + stepPeriod;
+        controller.Move(moveVector);
     }
 
     void Start()
     {
         // Get capsule collider
         collider = GetComponent<CapsuleCollider>();
-        rigidbody = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
         ttsManager = GameObject.FindObjectOfType<TTSManager>();
     }
 
     void Update()
     {
-        // Get move vector
-        Vector3 moveVector = getMoveVector();
+        // Get inputs
+        float x = Input.GetAxis("Vertical");
+        float y = Input.GetAxis("Horizontal");
 
-        Debug.Log(transform.position);
+        if (x == 0 && y == 0) {
+            // Stop moving
+            nextStepTime = -1;
+        } else {
+            // Do first step
+            if (nextStepTime == -1) {
+                doStep();
+            } else {
+                // Repeat for following steps
+                if (Time.time > nextStepTime) {
+                    doStep();
+                }
+            }
+        }
+    }
 
-        rigidbody.MovePosition(transform.position + moveVector);
-        
+    void OnControllerColliderHit() {
+        Debug.Log("Player collided with wall");
     }
 }
 
