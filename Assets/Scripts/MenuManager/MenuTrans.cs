981a558f542398fc5569e8c2bc7a1d383de2580d
GasbNandes <136932490+GabsNandes@users.noreply.github.com>
2024-06-14 16:35:06 -0300

    MenuUpdates

--- Assets/Scripts/MenuManager/MenuTrans.cs
+++ Assets/Scripts/MenuManager/MenuTrans.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class MenuTrans : MonoBehaviour
 {
@@ -12,7 +13,31 @@ public class MenuTrans : MonoBehaviour
 
     string mapLoad = MapLoader.map;
 
-    public InputActionProperty button;
+    public InputActionProperty showButton;
+
+    public GameObject menu;
+
+    public Transform player;
+
+    public GameObject controllerLeft;
+    public GameObject controllerRight;
+
+    
+    private XRInteractorLineVisual interactorLineVisualLeft;
+    private XRInteractorLineVisual interactorLineVisualRight;
+
+    public GameObject bengala;
+
+
+    public void exit(){
+
+        changeScene.quit();
+
+    }
+
+    public void mainMenu(){
+        changeScene.scene_changer("MainMenu", mapLoad);
+    }
 
 
     void Start()
@@ -25,24 +50,44 @@ public class MenuTrans : MonoBehaviour
             }else{
                 hasMenu = true;
             }
+
+        
+        interactorLineVisualLeft = controllerLeft.GetComponent<XRInteractorLineVisual>();
+        interactorLineVisualRight = controllerRight.GetComponent<XRInteractorLineVisual>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (hasMenu){
+        
+        if(showButton.action.WasPressedThisFrame() && hasMenu){
+
+            Debug.Log("hallo");
+            menu.SetActive(value: !menu.activeSelf);
             
-            if(button.action.WasPressedThisFrame()){
-                Debug.Log("hallo");
-                changeScene.scene_changer("MainMenu", mapLoad);
+            if(interactorLineVisualLeft.enabled && interactorLineVisualRight.enabled){
+
+                interactorLineVisualLeft.enabled = false;
+                interactorLineVisualRight.enabled = false;
+
+            }else{
+                interactorLineVisualLeft.enabled = true;
+                interactorLineVisualRight.enabled = true;
             }
             
-        }else{
-            
-            if(button.action.WasPressedThisFrame()){
-                Debug.Log("hallo");
-                changeScene.quit();
-            }
+
+            menu.transform.position = player.position+ new Vector3(x: player.forward.x, y: 0, z: player.forward.z).normalized*2;
+        
         }
+        
+        if(showButton.action.WasPressedThisFrame() && !hasMenu){
+            exit();
+        }
+
+        menu.transform.LookAt(worldPosition: new Vector3(x: player.position.x, y: menu.transform.position.y, z: player.position.z) );
+        menu.transform.forward *=-1;
+            
+        
     }
 }
